835874a6847d63e7d15cb63a0d418b7198e065dd
FlyRexy <eferovnik@mail.ru>
2022-12-29 13:31:44 +0300

    Final commit

--- spec/helpers/segment_helper_spec.rb
+++ spec/helpers/segment_helper_spec.rb
@@ -1,1 +1,14 @@
+# frozen_string_literal: true
 
+require 'rails_helper'
+
+RSpec.describe ServerHelper do
+  describe 'check result function' do
+    it 'good' do
+      expect(ServerHelper.solve('1 4 9 5 1')).to eq([['1 4 9', '1'], 0])
+    end
+    it 'invalid value' do
+      expect(ServerHelper.solve('sad')).to eq([["0"], 0])
+    end
+  end
+end
--- spec/requests/server_spec.rb
+++ spec/requests/server_spec.rb
@@ -1,1 +1,12 @@
+# frozen_string_literal: true
 
+require 'rails_helper'
+
+RSpec.describe ServerController, type: :controller do
+  describe 'check response format' do
+    it 'should be xml' do
+      get 'calculation', params: {arr: '1 4 9 5 1', num: '5', format: :xml }
+      expect(response.content_type).to eq('application/xml; charset=utf-8')
+    end
+  end
+end
